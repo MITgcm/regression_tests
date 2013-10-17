dbbed91c2661c8f779254bd1754d40d00e5bc128
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-10-17 01:45:23 +0000

    can also test a newer version (v14) of intel compiler

--- run_tests/other/intel_v12.sh
+++ run_tests/other/intel_v12.sh
@@ -1,44 +1,45 @@
 #! /bin/bash
 
+PROD_DIR="/opt/intel/composerxe-2011"
 if [ -z "${PATH}" ]
 then
-    PATH="/opt/intel/bin";
+    PATH="$PROD_DIR/bin";
 else
-    PATH="/opt/intel/bin:${PATH}";
+    PATH="$PROD_DIR/bin:${PATH}";
 fi
     PATH="/opt/intel/mpich-1.2.7p1_ic-12.0.4/bin:${PATH}"
     export PATH
-    MPI_INC_DIR="/opt/intel/mpich-1.2.7p1/include"
+    MPI_INC_DIR="/opt/intel/mpich-1.2.7p1_ic-12.0.4/include"
     export MPI_INC_DIR
 
   if [ -z "${LD_LIBRARY_PATH}" ]
    then
-       LD_LIBRARY_PATH="/opt/intel/lib/intel64"
+       LD_LIBRARY_PATH="$PROD_DIR/lib/intel64"
    else
-       LD_LIBRARY_PATH="/opt/intel/lib/intel64:${LD_LIBRARY_PATH}"
+       LD_LIBRARY_PATH="$PROD_DIR/lib/intel64:${LD_LIBRARY_PATH}"
    fi
    export LD_LIBRARY_PATH
 
    if [ -z "${LIBRARY_PATH}" ]
    then
-       LIBRARY_PATH="/opt/intel/lib/intel64"
+       LIBRARY_PATH="$PROD_DIR/lib/intel64"
    else
-       LIBRARY_PATH="/opt/intel/lib/intel64:${LIBRARY_PATH}"
+       LIBRARY_PATH="$PROD_DIR/lib/intel64:${LIBRARY_PATH}"
    fi
    export LIBRARY_PATH
    if [ -z "${NLSPATH}" ]
    then
-     NLSPATH="/opt/intel/lib/intel64/locale/%l_%t/%N"
+     NLSPATH="$PROD_DIR/lib/intel64/locale/%l_%t/%N"
    else
-     NLSPATH="/opt/intel/lib/intel64/locale/%l_%t/%N:${NLSPATH}"
+     NLSPATH="$PROD_DIR/lib/intel64/locale/%l_%t/%N:${NLSPATH}"
    fi
    export NLSPATH
 
 if [ -z "${MANPATH}" ]
 then
-    MANPATH="/opt/intel/man/en_US":$(manpath)
+    MANPATH="$PROD_DIR/man/en_US":$(manpath)
 else
-    MANPATH="/opt/intel/man/en_US:${MANPATH}"
+    MANPATH="$PROD_DIR/man/en_US:${MANPATH}"
 fi
 export MANPATH
 
