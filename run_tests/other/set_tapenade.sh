1553145ad31c0d5c209a22baf2e02995fedeffc8
Jean-Michel Campin <jmc@ocean.mit.edu>
2023-06-09 02:16:49 +0000

    for completeness, add this very short script just to get "tapenade" in the PATH

---
+++ run_tests/other/set_tapenade.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# $Header:  $
+# $Name:  $
+
+#   PATH="/home/jmc/install/tapenade_3.16/bin:${PATH}";
+   #PATH="${PATH}:/home/jmc/install/tapenade_3.16/bin";
+    PATH="${PATH}:/home/jmc/install/tapenade_3.16.v2/bin";
+    export PATH
