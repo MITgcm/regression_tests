b08f0cdca6992b87cdbbefac8085ffa1226f13c1
Jean-Michel Campin <jmc@mit.edu>
2025-09-29 13:46:17 -0400

    update env.var. setting scripts
    - add a very short script to set Tapenade env.var.
    - remove un-used OpenAD scripts

---
+++ run_tests/ref_machine/set_tapenade.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#- set environnement variables to use locally installed Tapenade
+
+    PATH="${PATH}:/home/jm_c/install/tapenade_3.16/bin";
+   #PATH="${PATH}:/home/jm_c/install/tapenade_3.16.v2/bin";
+    export PATH
